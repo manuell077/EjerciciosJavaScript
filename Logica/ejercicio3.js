844d4fd9701c20eff39d684476b8e6c958bcc273
Manuel Ardila <ardilasanchezmanuelsteven@gmail.com>
2024-12-02 09:46:58 -0500

    Ejercicio 3 terminado

--- Logica/ejercicio3.js
+++ Logica/ejercicio3.js
@@ -1,15 +1,24 @@
-let num1 = prompt("Dame el primer numero");
-let num2 = prompt("Dame el segundo numero");
+let num1 = parseInt(prompt("Dame el primer numero")); //El usuario ingresa el primer numero
+let num2 = parseInt (prompt("Dame el segundo numero")); //El usuario ingresa el segundo numero
 
 
 
-try{
+try{ //manejamos execepciones con try 
 
-let division = (num1/num2);
+let division = (num1/num2); //Ejecutamos la division
 
-alert("El resultado de la division es " + division);
 
-}catch(error){ 
+
+if(num2 !== 0 && num2 > 0){ //Validamos si el divisor es diferente a 0 y el numero es positivo 
+alert("El resultado de la division es " + division);//Donde llegue a ser true imprimimos el siguiente mensaje
+
+}else{ //de lo contrario
+
+    throw "El divisor no puede ser 0";//Hacemos un error personalizado
+}
+
+
+}catch(error){  //Error personalizado 
     
-    alert("El divisor no puede ser cero " + error.message);
+    alert(error); //Mostramos el error personalizado 
 }
---
+++ Logica/ejercicio4.js
@@ -0,0 +1,11 @@
+let numero = parseInt(prompt("Ingresa un numero entero")); // Pedimos al usuario que ingrese un numero ya sea par o impar
+
+
+if(numero % 2 === 0){
+
+
+    alert("EL numero es par ");
+}else{
+
+    alert("El numero es impar")
+}
