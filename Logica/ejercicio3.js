0d94b9d42a2ca225482ff2d593f95ce0b8b6d93f
Manuel Ardila <ardilasanchezmanuelsteven@gmail.com>
2024-12-02 09:54:30 -0500

    Ejercicio 4 terminado

--- Logica/ejercicio3.js
+++ Logica/ejercicio3.js
@@ -9,7 +9,7 @@ let division = (num1/num2); //Ejecutamos la division
 
 
 
-if(num2 !== 0 && num2 > 0){ //Validamos si el divisor es diferente a 0 y el numero es positivo 
+if(num2 !== 0 ){ //Validamos si el divisor es diferente a 0 y el numero es positivo 
 alert("El resultado de la division es " + division);//Donde llegue a ser true imprimimos el siguiente mensaje
 
 }else{ //de lo contrario
--- Logica/ejercicio4.js
+++ Logica/ejercicio4.js
@@ -1,11 +1,10 @@
 let numero = parseInt(prompt("Ingresa un numero entero")); // Pedimos al usuario que ingrese un numero ya sea par o impar
 
 
-if(numero % 2 === 0){
+if(numero % 2 === 0){ //Validamos si el numero es par
 
-
-    alert("EL numero es par ");
+ 
+    alert("EL numero es par "); //Imprimimos el numero par
 }else{
 
-    alert("El numero es impar")
-}
+    alert("El numero es impar"); }//Caso contario que imprima que no es par
