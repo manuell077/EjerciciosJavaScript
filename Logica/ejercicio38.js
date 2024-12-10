ccfc28d03d760d8bab955e4cf9b7451f238fd2be
unknown <manuell077>
2024-12-10 00:52:00 -0500

    ejercicio 38

---
+++ Logica/ejercicio38.js
@@ -0,0 +1,14 @@
+//Primero ingresams los datos que nos dan 
+
+
+//La parte de martin es 2/3
+
+let martin = 2/3; //esto da 0.6667
+let jairo = 1/4; //esto da 0.25
+
+let lorena = 1 - (martin+jairo); //Tenemos que poner 1 ya que representa el total de las tapas , ya que en frracciones el 1 representa  el 100%
+
+alert("La parte de lorena es " + lorena);
+
+
+
