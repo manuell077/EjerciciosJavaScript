0ca891eb846feeb0abd94d3bec293a57bde5ea3e
unknown <manuell077>
2024-12-10 15:07:05 -0500

    ejercicio 45

---
+++ Logica/ejercicio45.js
@@ -0,0 +1,21 @@
+//Pedimos que ingrese un numero 
+
+let numero = parseInt(prompt("Ingresa un numero"));
+
+
+//Validamos si el numero que ingreso es positivo y menor que 100
+
+if(numero > 0 && numero <= 100){
+
+    alert("El numero cumple con las caracteristicas");
+}else{
+    
+    alert("El numero no cumple con las caracteristicas");
+
+
+
+}
+
+
+
+
