436374c2e0b715e77ec46f4d756aa12b44317559
unknown <manuell077>
2024-12-11 19:43:55 -0500

    ejercicio 63

---
+++ Logica/ejercicio63.js
@@ -0,0 +1,10 @@
+//Le pedimos al usuario su edad 
+
+let edad = parseInt(prompt("Ingresa la edad del usuario"));
+
+//Ahora con un bucle for imprimiremos todos los años que ha cumplido
+
+for(let i = 1; i<= edad ; i++){
+    
+    alert("Has cumplido " +  i + " año");//Imprimimos los años cumplidos
+}
